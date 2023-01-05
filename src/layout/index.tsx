873b3e8d1234cd91d90bd607258a7c2b7781646b
rossnerandre <rossnerandre@gmail.com>
2023-01-05 17:29:38 -0300

    Add style to main content layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,11 +1,21 @@
 import { Header } from "./Header";
 import { Outlet } from "react-router-dom";
+import { styled } from "@stitches/react";
+
+const Main = styled("main", {
+  minHeight: "100vh",
+  textAlign: 'center',
+  marginTop: 40,
+  padding: '0 80px'
+});
 
 function MyLayout() {
   return (
     <>
       <Header />
-      <Outlet />
+      <Main>
+        <Outlet />
+      </Main>
     </>
   );
 }
