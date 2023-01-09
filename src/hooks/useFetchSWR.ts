7d710812bd463ade9e15f7629492ffe0bc2904b2
rossnerandre <rossnerandre@gmail.com>
2023-01-09 11:12:50 -0300

    Remove return data.data

--- src/hooks/useFetchSWR.ts
+++ src/hooks/useFetchSWR.ts
@@ -1,9 +1,9 @@
 import useSWR from "swr";
 import api from "../services/api";
 
-export function useFetchSWR<Data = any>(url: string) {
+export function useFetchSWR<Data = any>(url: string) {  
   const { data, error, isLoading, mutate } = useSWR<Data>(url, (url) =>
-    api.get(url).then((data) => data.data)
+    api.get(url)
   );
 
   return { data, error, isLoading, mutate };
