07e869d856a61be1afe29f5743259698deee8260
rossnerandre <rossnerandre@gmail.com>
2023-01-09 17:09:02 -0300

    Add pagination, order, switch, and button icons

--- src/components/TableTodo.tsx
+++ src/components/TableTodo.tsx
@@ -1,29 +1,41 @@
-import { useState, useRef, useEffect } from "react";
-import { Space, Table, Tag } from "antd";
+import { useState, useRef, useEffect, useCallback } from "react";
+import { Button, Popconfirm, Space, Switch, Table, Tag } from "antd";
 import type { ColumnsType } from "antd/es/table";
 import ModalForm, { ModalHandles } from "./modal-form/ModalForm";
 import { useTranslation } from "react-i18next";
 import { TodoType } from "../types/todo";
 import { useFetchSWR } from "../hooks/useFetchSWR";
 import api from "../services/api";
+import dayjs from "dayjs";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 
 function TableTodo() {
   const [editData, setEditData] = useState<TodoType | null>();
   const { t } = useTranslation();
 
+  const [order, setOrder] = useState<string>("desc");
+  const [orderColumn, setOrderColumn] = useState<string>("create_at");
   const [currentPage, setCurrentPage] = useState<number>(1);
   const { data, isLoading, mutate } = useFetchSWR(
-    `todos/?_page=${currentPage}`
+    `todos/?_sort=${orderColumn}&_order=${order}&_page=${currentPage}`
   );
-  const [dataTeste, setDataTeste] = useState<TodoType[]>([]);
+  const [dataTodo, setDataTodo] = useState<TodoType[]>([]);
   const [totalPages, setTotalPages] = useState<number>();
+
+  const orders = useCallback(
+    (local: string, order: any) => {
+      setOrderColumn(local);
+      setOrder(order === "descend" ? "desc" : "asc");
+    },
+    [order, orderColumn]
+  );
 
   useEffect(() => {
     if (data) {
-      setDataTeste(data.data);
+      setDataTodo(data.data);
       setTotalPages(+data.headers.get("x-total-count"));
     }
-  }, [totalPages, data]);
+  }, [totalPages, data, dataTodo]);
 
   const modalRef = useRef<ModalHandles>(null);
 
@@ -57,27 +69,70 @@ function TableTodo() {
       title: `${t("todo")}`,
       dataIndex: "todo",
       key: "todo",
-      render: (text) => <a>{text}</a>,
+      sorter: (a, b, c) => {
+        orders("todo", c);
+        return 0;
+      },
+      render: (text, record) => {
+        return !record.completed ? <p>{text}</p> : <s>{text}</s>;
+      },
+    },
+    {
+      title: `${t("Date To Make")}`,
+      dataIndex: "dateDoTodo",
+      key: "dateDoTodo",
+      sorter: (a, b, c) => {
+        orders("dateDoTodo", c);
+        return 0;
+      },
+      render: (text) => {
+        const dateFormat = dayjs(+text).format("DD/MM/YYYY HH:mm:ss");
+        return <p>{dateFormat}</p>;
+      },
     },
     {
       title: "Action",
       key: "action",
       render: (_, record) => (
         <Space size="middle">
-          <a onClick={() => handleEditRow(record)}>{t("editTodo")}</a>
-          <a onClick={() => handleDelete(record)}>{t("Delete")}</a>
-          <a onClick={() => handleCompleted(record)}>{t("Completed?")}</a>
+          <Popconfirm
+            title="Sure to delete?"
+            onConfirm={() => handleDelete(record)}
+          >
+            <Button danger icon={<DeleteOutlined />}>
+              {t("Delete")}
+            </Button>
+          </Popconfirm>
+          <Button
+            onClick={() => handleEditRow(record)}
+            icon={<EditOutlined />}
+            disabled={record.completed}
+          >
+            {t("editTodo")}
+          </Button>
+          <Switch
+            disabled={record.completed}
+            checked={record.completed}
+            onClick={() => handleCompleted(record)}
+          />
         </Space>
       ),
     },
   ];
 
   return (
-    <>
+    <div style={{ display: "flex", justifyContent: "center" }}>
       <Table
-        columns={columns}        
-        dataSource={dataTeste}        
+        style={{ width: "700px" }}
+        bordered
+        columns={columns}
+        tableLayout="auto"
+        dataSource={dataTodo}
         loading={isLoading}
+        onChange={(record) => {
+          console.log(record);
+        }}
+        // rowClassName={(record) => (record.completed ? "disabled-row" : "")}
         pagination={{
           pageSize: 10,
           total: totalPages,
@@ -87,7 +142,7 @@ function TableTodo() {
         }}
       />
       <ModalForm ref={modalRef} dataTodo={editData} />
-    </>
+    </div>
   );
 }
 
