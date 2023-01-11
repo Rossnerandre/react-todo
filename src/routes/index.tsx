b17635c0f7aee61fbbf4a7f22c559c025e954646
rossnerandre <rossnerandre@gmail.com>
2023-01-11 16:59:03 -0300

    Add Protected Function

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,23 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import MyLayout from "../layout";
 import App from "../App";
+import Login from "../pages/login/Login";
+
+import { ReactNode } from "react";
+import useLoginStore from "../store/loginStore";
+import Register from "../pages/login/Register";
+
+type AuthGuardProps = {
+  children: ReactNode;
+};
+
+export function Protected({ children }: AuthGuardProps) {
+  const { isAuthenticated } = useLoginStore();
+  if (!isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+  return <>{children}</>;
+}
 
 const router = createBrowserRouter([
   {
@@ -9,7 +26,19 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <App />,
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/todos",
+        element: (
+          <Protected>
+            <App />
+          </Protected>
+        ),
       },
     ],
   },
