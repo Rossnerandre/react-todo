d1552014c6cfbbc2cf67443939ccda221e2620c1
rossnerandre <rossnerandre@gmail.com>
2023-01-11 10:49:07 -0300

    Config darkTheme

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,27 +1,44 @@
 import { useTranslation } from "react-i18next";
 import { styled } from "@stitches/react";
-import { light } from "../theme/light";
-import { dark } from "../theme/dark";
 import useThemeStore from "../store/themeStore";
 import { Switch } from "antd";
+import { theme, Typography } from "antd";
+const { useToken } = theme;
+const { Title } = Typography;
+import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
 
 const StyledHeader = styled("header", {
-  backgroundColor: "$primary",
   height: 82,
   display: "flex",
-  justifyContent: "center",
+  justifyContent: "space-around",
   alignItems: "center",
-  color: "$text",
+  flexBasis: "50%",
 });
 
 export function Header() {
   const store = useThemeStore();
   const { t } = useTranslation();
+  const { token } = useToken();
 
   return (
-    <StyledHeader className={store.theme === "light" ? light : dark}>
-      <h1>{t("header")}</h1>
-      <Switch onChange={() => store.setTheme()} />
+    <StyledHeader
+      style={{
+        backgroundColor: `${
+          store.theme === "light" ? token.colorPrimary : token.colorPrimaryBg
+        }`,
+      }}
+    >
+      <div>
+        <Title level={2}>{t("header")}</Title>
+      </div>
+      <div>
+        <Switch
+          checkedChildren={<MdOutlineLightMode />}
+          unCheckedChildren={<MdOutlineDarkMode />}
+          onChange={() => store.setTheme()}
+          checked={store.theme === "light" ? true : false}
+        />
+      </div>
     </StyledHeader>
   );
 }
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,26 +1,37 @@
 import { Header } from "./Header";
 import { Outlet } from "react-router-dom";
 import { styled } from "@stitches/react";
-import { light } from "../theme/light";
-import { dark } from "../theme/dark";
 import useThemeStore from "../store/themeStore";
+import { ConfigProvider, theme } from "antd";
+const { defaultAlgorithm, darkAlgorithm, useToken } = theme;
 
 const Main = styled("main", {
-  background: "$background",
-  minHeight: "100vh",
+  minHeight: "calc(100vh - 82px)",
+  width: "100%",
   textAlign: "center",
-  padding: "40px 80px",
+  padding: "40px 0",
 });
 
 function MyLayout() {
   const store = useThemeStore();
+  const { token } = useToken();
   return (
-    <>
+    <ConfigProvider
+      theme={{
+        algorithm: store.theme === "light" ? defaultAlgorithm : darkAlgorithm,
+      }}
+    >
       <Header />
-      <Main className={store.theme === "light" ? light : dark}>
+      <Main
+        style={{
+          backgroundColor: `${
+            store.theme === "light" ? token.colorPrimaryBg : "#333"
+          }`,
+        }}
+      >
         <Outlet />
       </Main>
-    </>
+    </ConfigProvider>
   );
 }
 
