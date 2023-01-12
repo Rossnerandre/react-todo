2b9ed998b3d6cebac0d00302c494fa0f2134d319
rossnerandre <rossnerandre@gmail.com>
2023-01-12 13:51:30 -0300

    Add hooks to useAuth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,56 +1,64 @@
 import api from "../services/api";
 import useLoginStore from "../store/loginStore";
+import { v4 as uuidv4 } from "uuid";
+import useLocalStorage from "./useLocalStorage";
 
 export default function useAuth() {
   const { setAuthenticated } = useLoginStore();
+  const { getItems, setItems } = useLocalStorage();
 
   const autoAuth = () => {
-    const nicknameUser = localStorage.getItem("username");
-    const isAuthenticated = localStorage.getItem("isAuthenticated");
-    if (nicknameUser && isAuthenticated === "true") {
-      setAuthenticated(nicknameUser, true);
+    const localItems = getItems(["username", "isAuthenticated", "idUser"]);
+    if (localItems[0] && localItems[1] === "true" && localItems[2]) {
+      setAuthenticated(localItems[0], true, localItems[2]);
       return true;
     }
     return false;
   };
 
   const login = async (email: string, password: string) => {
-    const response = await api
-      .get(`users?email=${email}`)
-      .then((data) => data.data);
-    if (response.length > 0) {
-      if (response[0].password == password) {
-        localStorage.setItem("username", response[0].user);
-        localStorage.setItem("isAuthenticated", "true");
-        setAuthenticated(response[0].user, true);
+    const { data } = await api.get(`users?email=${email}`);
+    if (data.length > 0) {
+      if (data[0].password == password) {
+        setItems([
+          { key: "username", value: data[0].user },
+          { key: "idUser", value: data[0].id },
+          { key: "isAuthenticated", value: "true" },
+        ]);
+        setAuthenticated(data[0].user, true, data[0].id);
+        return data.status;
       }
-      return response.status;
+      throw new Error("Email or pass don't match");
     }
     throw new Error("Email or pass don't match");
   };
 
   const register = async (
     email: string,
-    password: string,
-    username: string
+    username: string,
+    password: string
   ) => {
-    const verifiEmail = await api
-      .get(`users?email=${email}`)
-      .then((data) => data.data);
-    if (verifiEmail.length >= 1) {
+    const { data } = await api.get(`users?email=${email}`);
+    if (data.length >= 1) {
       throw new Error("E-mail is registred!");
     }
+
+    const id = uuidv4();
 
     const response = await api.post("users", {
       user: `${username}`,
       email: `${email}`,
       password: `${password}`,
+      id: `${id}`,
     });
 
-    localStorage.setItem("username", username);
-    localStorage.setItem("isAuthenticated", "true");
+    setItems([
+      { key: "username", value: username },
+      { key: "idUser", value: id },
+      { key: "isAuthenticated", value: "true" },
+    ]);
 
-    setAuthenticated(username, true);
+    setAuthenticated(username, true, id);
 
     return response;
   };
